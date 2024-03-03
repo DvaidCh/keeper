c6d5624d09a31a1b27022214b7c12b4f10333dd9
DvaidCh <89460195+DvaidCh@users.noreply.github.com>
2024-03-03 14:53:34 +0200

    little enchance
    takes more than 1 parametr
    now exit only with Ctrl+C

--- keep.cpp
+++ keep.cpp
@@ -2,15 +2,24 @@
 #include <cstdlib>
 #include <string> 
 int main(int argc, char *argv[]){
-	std::string gate=argv[1];
+	std::string gate="";
+	//=argv[1];
+	{
+		std::string temp;
+		for(int i=1;i<argc;i++){
+			temp=argv[i];
+			gate+=(temp)+" ";
+		}
+	}
+	
 	std::string command;
 	system("");
-	std::cout<<"you now keeped in the \033[1;32m"<<gate<<"\033[0m\n";
-	std::cout<<"to exit write\033[1m exit\033[0m\n";
+	std::cout<<"you now kept in the \033[1;32m"<<gate<<"\033[0m\n";
+	std::cout<<"to exit press\033[1m Ctrl+C\033[0m\n";
 	while(true){
-		std::cout<<"\033[1;32m"<<gate<<">\033[0m";
+		std::cout<<"\033[1;32m"<<gate<<"\033[0m";
 		std::cin>>command;
-		if(!command.compare("exit"))break;
-		system((gate+" "+command).c_str());
+		
+		system((gate+command).c_str());
 	}
 }
